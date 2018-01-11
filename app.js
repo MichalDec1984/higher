7f3ff59e863b10a23b921372f41f3b9f6dc14b5e
Michał <michaldec84@gmail.com>
2018-01-11 20:57:04 +0100

    solution added

---
+++ app.js
@@ -0,0 +1,47 @@
+$(function() {
+  //variables for url
+  var imgUrl = 'https://picsum.photos/list';
+
+
+  /* Insert images to DOM  */
+  function insertContent(images) {
+    $.each(images, function(indexImg, img) {
+    if (indexImg <3) {
+    	$.ajax({
+    		url: img.post_url,
+    		type: 'GET',
+    		success: function(data) {
+        var html = $.parseHTML( data ), 
+            img = $(html).find("img:eq( 1 )" ),
+            len = img.length; 
+            if( len > 0 ){
+                var src = img.attr("src"); // get id of first image
+            } else {
+                console.log("Image not found");
+            }
+            console.log(src);
+            var img = $('<img id="img">');
+						img.appendTo('.img');
+   					img.attr("src", src);
+    		}
+		 });	
+    }  
+   });
+  }
+
+  /* Load images and insert them into the DOM
+  */
+  function loadImages() {
+        $.ajax({
+          	url: imgUrl
+        }).done(function(response){
+     		    insertContent(response)
+            //console.log(response);
+    	 }).fail(function(error) {
+           //console.log(error)
+       })
+  }
+
+  loadImages();
+
+});
